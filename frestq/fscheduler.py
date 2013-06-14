f4e7d9381fa13652841c078ab249448aaf73dd87
Eduardo Robles Elvira <edulix@wadobo.com>
2013-06-14 22:19:52 +0200

    fixing license headers

--- frestq/fscheduler.py
+++ frestq/fscheduler.py
@@ -4,17 +4,17 @@
 # This file is part of frestq.
 # Copyright (C) 2013  Eduardo Robles Elvira <edulix AT wadobo DOT com>
 
-# election-orchestra is free software: you can redistribute it and/or modify
+# frestq is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License.
 
-# election-orchestra  is distributed in the hope that it will be useful,
+# frestq  is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 
 # You should have received a copy of the GNU Lesser General Public License
-# along with election-orchestra.  If not, see <http://www.gnu.org/licenses/>.
+# along with frestq.  If not, see <http://www.gnu.org/licenses/>.
 
 from apscheduler.scheduler import Scheduler
 from apscheduler.util import convert_to_datetime
