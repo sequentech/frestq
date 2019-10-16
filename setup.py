7e178cbc2506af475be068f31fcfd5f6c9ff2ede
Eduardo Robles Elvira <edulix@nvotes.com>
2019-10-16 17:39:24 +0200

    allowing newer versions of pip

--- setup.py
+++ setup.py
@@ -13,7 +13,10 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with frestq.  If not, see <http://www.gnu.org/licenses/>.
 from setuptools import setup
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
 install_reqs = parse_requirements("requirements.txt")
