2800b8756021db2b28557fcd7d48d3b8174a329c
Eduardo Robles Elvira <edulix@nvotes.com>
2020-12-30 09:26:47 +0100

    fix reference to requirement

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ install_reqs = parse_requirements("requirements.txt", session='hack')
 
 # reqs is a list of requirement
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-reqs = [str(ir.req) for ir in install_reqs]
+reqs = [str(ir.requirement) for ir in install_reqs]
 
 setup(
     name='frestq',
