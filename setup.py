1bba6ab1e39bb2d13ac90b313f25c0a859017f52
Eduardo Robles Elvira <edulix@gmail.com>
2021-11-29 16:31:14 +0100

    Release for version 5.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='frestq',
-    version='4.0.0',
+    version='5.0.0',
     author='Agora Voting SL',
     author_email='contact@nvotes.com',
     packages=['frestq'],
