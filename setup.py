8812f4863185beca092960dbd5f42ec7a31af543
Eduardo Robles Elvira <edu@sequentech.io>
2022-05-13 14:44:16 +0200

    Release for version 5.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='frestq',
-    version='5.1.0',
+    version='5.1.1',
     author='Agora Voting SL',
     author_email='contact@nvotes.com',
     packages=['frestq'],
