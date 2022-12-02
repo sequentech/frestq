cbf15224fcda3fca71ad43db06f53673caf88bbd
Findeton <felix@sequentech.io>
2022-12-02 11:32:24 -0500

    Release for version 7.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='frestq',
-    version='7.0.0-beta.1',
+    version='7.1.0',
     author='Sequent Tech Inc',
     author_email='legal@sequentech.io',
     packages=['frestq'],
