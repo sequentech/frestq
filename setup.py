504248ec18f9f38d342bec0386132a48ef3224a7
Eduardo Robles Elvira <edu@sequentech.io>
2023-04-11 17:28:13 +0200

    Release for version 7.3.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='frestq',
-    version='7.2.0',
+    version='7.3.0',
     author='Sequent Tech Inc',
     author_email='legal@sequentech.io',
     packages=['frestq'],
