d460a5320f03c8585e2f30ecdb73a091d02a49dc
Eduardo Robles Elvira <edu@sequentech.io>
2023-10-19 17:04:17 +0200

    Release for version 10.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='frestq',
-    version='10.0.1',
+    version='10.0.2',
     author='Sequent Tech Inc',
     author_email='legal@sequentech.io',
     packages=['frestq'],
