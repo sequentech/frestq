97fc30447f8e46a1414064378934f6bb22d470aa
Findeton <felix@sequentech.io>
2024-09-10 18:34:38 -0500

    Release for version 10.5.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='frestq',
-    version='10.4.2',
+    version='10.5.1',
     author='Sequent Tech Inc',
     author_email='legal@sequentech.io',
     packages=['frestq'],
